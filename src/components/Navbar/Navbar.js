3a36ebd80cb9a1f3328779c7f55b9586da56bdf4
Gian Piero Valenti <gpvalentip@gmail.com>
2022-12-20 17:16:29 -0300

    segunda preentrega

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,24 +1,29 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg bg-light">
     <div className="container-fluid">
-      <a className="navbar-brand" href="#">HOME</a>
+      <NavLink to='/'>
+        <img src='./logopet.jpg'  width='70' height='70'/>
+      </NavLink>
+
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
+        <ul className="navbar-nav mx-auto">
           <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="#">PRODUCTOS</a>
+            <NavLink className="nav-link" to='/'>HOME</NavLink>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">GALERIA</a>
+            <NavLink className="nav-link" to='/productos'>PRODUCTOS</NavLink>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">CONTACTO</a>
+            <NavLink className="nav-link" to='/galeria'>GALERIA</NavLink>
           </li>
+         
           <li className="nav-item">
             <a className="nav-link disabled">PET SHOP STORE </a>
           </li>
